66d0856c134964bfa907e99aa901d85fce2a6a32
alejandro.ariza <alejandro.ariza@gtmotive.com>
2024-09-11 16:46:07 +0200

    add menu and google maps integration

---
+++ source/src/pages/map.js
@@ -0,0 +1,15 @@
+function initMap() {
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 37.88547, lng: -4.779306 },
+      mapId: 'ed71ed6ac9dcac8f',
+      zoom: 13
+    });
+  
+    const marker = new google.maps.marker.AdvancedMarkerElement({
+      position: { lat: 37.8854267, lng: -4.779344621 },
+      map: map,
+      title: '18Ctea Cordoba'
+    });
+  }
+  
+  window.initMap = initMap;
